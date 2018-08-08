e5f8012440c07f4649107eaeb293f56982791dc9
James Wetter <wetter.j@gmail.com>
2018-08-08 13:04:26 +0900

    remove depricated files

--- routes/api/user/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const controller = require('./user.controller');
-
-router.get('/', controller.get);
-router.post('/', controller.post);
-
-module.exports = router;
