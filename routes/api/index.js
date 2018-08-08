27f809c9b87fc3bd5046390a5d95c075a7b30e5e
James Wetter <wetter.j@gmail.com>
2018-08-08 13:11:42 +0900

    restored user controller

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,6 @@
 const router = require('express').Router();
-const auth = require('./auth');
 const user = require('./user');
-const campaign = require('./campaign');
 
-router.use('/auth', auth);
 router.use('/user', user);
-router.use('/campaign', campaign);
 
 module.exports = router;
