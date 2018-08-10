a13d1025192427bad59f95c8207a9df05d220930
James Wetter <wetter.j@gmail.com>
2018-08-10 14:41:08 +0900

    update import for auth

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import controller from './auth.controller';
+import * as controller from './auth.controller';
 
 const router = express.Router();
 router.post('/', controller.post);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import users from './users';
-// import auth from './auth';
+import auth from './auth';
 
 const router = express.Router();
 router.use('/users', users);
-// router.use('/auth', auth);
+router.use('/auth', auth);
 
 export default router;
