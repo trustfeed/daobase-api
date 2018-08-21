71e633fdd528a59a82ae37ac39de280c06410aec
James Wetter <wetter.j@gmail.com>
2018-08-21 15:23:35 +0900

    try addig the region

--- models/s3.js
+++ models/s3.js
@@ -14,6 +14,7 @@ export const signUpload = (campaignId, prefix) => {
     Key: prefix + '/' + campaignId + '-' + rand,
     Expires: 60,
     ContentType: 'application/pdf',
+    Region: config.region.trim(),
   };
   return new Promise((resolve, reject) => {
     s3.getSignedUrl('putObject', params, (err, url) => {
