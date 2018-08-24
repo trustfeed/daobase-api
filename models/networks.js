f339b382c173d247cd8994d5dc42da0a6ccaea65
James Wetter <wetter.j@gmail.com>
2018-08-24 16:50:46 +0900

    switch back to infura

--- models/networks.js
+++ models/networks.js
@@ -4,7 +4,8 @@ import config from '../config';
 const web3OnNetwork = (network) => {
   switch (network) {
   case 'rinkeby':
-    return new Web3(config.rinkebyNode);
+    return new Web3(`https://rinkeby.infura.io/${config.infuraKey}`);
+  // return new Web3(config.rinkebyNode);
   // case 'local':
   //  return new Web3('ws://localhost:7545');
   // case 'ganache-trustfeed':
