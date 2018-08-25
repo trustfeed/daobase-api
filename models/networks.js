42c8690a75cdf7ac18864f9192d044844fdb697b
James Wetter <wetter.j@gmail.com>
2018-08-25 13:46:43 +0900

    fix the port for hosted node

--- models/networks.js
+++ models/networks.js
@@ -4,19 +4,15 @@ import config from '../config';
 const web3OnNetwork = (network) => {
   switch (network) {
   case 'rinkeby':
-    return new Web3(`https://rinkeby.infura.io/${config.infuraKey}`);
-    // let prov = new Web3.providers.WebsocketProvider(
-    //  config.reinkebyNode,
-    //  {
-    //    headers: {
-    //      Origin: 'localhost',
-    //    },
-    //  });
-    // return new Web3(prov);
-  // case 'local':
-  //  return new Web3('ws://localhost:7545');
-  // case 'ganache-trustfeed':
-  //  return new Web3('https://ganache.trustfeed.io');
+    // return new Web3(`https://rinkeby.infura.io/${config.infuraKey}`);
+    let prov = new Web3.providers.WebsocketProvider(
+      config.reinkebyNode,
+      {
+        headers: {
+          Origin: 'localhost',
+        },
+      });
+    return new Web3(prov);
   default:
     throw new Error('unknown network');
   }
