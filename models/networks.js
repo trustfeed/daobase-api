a5d40ddb16d8c8d79b6cd6046b26ccd908b2058b
James Wetter <wetter.j@gmail.com>
2018-08-25 17:53:36 +0900

    try local geth with another EC2 instance

--- models/networks.js
+++ models/networks.js
@@ -5,27 +5,23 @@ import * as te from '../typedError';
 const web3OnNetwork = async (network) => {
   switch (network) {
   case 'rinkeby':
-    return new Web3(`https://rinkeby.infura.io/${config.infuraKey}`);
-    //let prov = new Web3.providers.WebsocketProvider(
-    //  config.rinkebyNode,
-    //  {
-    //    headers: {
-    //      Origin: 'api.tokenadmin.work',
-    //    },
-    //  });
-    //let w3 = new Web3(prov);
-    //// if (w3.isConnected()) {
-    //return w3.eth.getBlockNumber()
-    //  .then(() => {
-    //    return w3;
-    //  })
-    //  .catch(err => {
-    //    console.log(err);
-    //    throw new te.TypedError(500, 'cannot connect to web3');
-    //  });
-    //// } else {
-    ////  throw new te.TypedError(500, 'cannot connect to web3');
-    //// }
+    // return new Web3(`https://rinkeby.infura.io/${config.infuraKey}`);
+    let prov = new Web3.providers.WebsocketProvider(
+      config.rinkebyNode,
+      {
+        headers: {
+          Origin: 'api.tokenadmin.work',
+        },
+      });
+    let w3 = new Web3(prov);
+    return w3.eth.getBlockNumber()
+      .then(() => {
+        return w3;
+      })
+      .catch(err => {
+        console.log(err);
+        throw new te.TypedError(500, 'cannot connect to web3');
+      });
   default:
     throw new Error('unknown network');
   }
