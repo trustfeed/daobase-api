b78b11e87979262237496407c9e45a219dac374c
James Wetter <wetter.j@gmail.com>
2018-08-27 09:17:49 +0900

    try local node, fall back onto infura

--- models/networks.js
+++ models/networks.js
@@ -5,22 +5,22 @@ import * as te from '../typedError';
 const web3OnNetwork = async (network) => {
   switch (network) {
   case 'rinkeby':
-    //return new Web3(`https://rinkeby.infura.io/${config.infuraKey}`);
-     let prov = new Web3.providers.WebsocketProvider(
+    // return new Web3(`https://rinkeby.infura.io/${config.infuraKey}`);
+    let prov = new Web3.providers.WebsocketProvider(
       config.rinkebyNode,
       {
         headers: {
           Origin: 'api.tokenadmin.work',
         },
       });
-     let w3 = new Web3(prov);
-     return w3.eth.getBlockNumber()
+    let w3 = new Web3(prov);
+    return w3.eth.getBlockNumber()
       .then(() => {
         return w3;
       })
       .catch(err => {
         console.log(err);
-        throw new te.TypedError(500, 'cannot connect to web3');
+        return new Web3(`https://rinkeby.infura.io/${config.infuraKey}`);
       });
   default:
     throw new Error('unknown network');
