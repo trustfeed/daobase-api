b2badc58031b64d9c4678f24c1f71639f48594d9
James Wetter <wetter.j@gmail.com>
2018-10-01 17:16:07 +0900

    remove old controllers

--- src/controllers/investments/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as controller from './investments.controller';
-import authMiddleware from '../../middleware/auth';
-
-const router = express.Router();
-router.use('/', authMiddleware);
-router.get('/', controller.get);
-
-export default router;
